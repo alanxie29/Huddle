857c1c22fafef5df48eb5a634aa3f0da8f233619
alanxie29 <alanxie29@gmail.com>
2019-03-07 11:00:37 -0500

    location services

--- src/components/HomeComponent/Home/Home.js
+++ src/components/HomeComponent/Home/Home.js
@@ -1,9 +1,39 @@
 import React, { Component } from 'react';
 import { Text, View, StyleSheet } from 'react-native';
+import { Constants, MapView, Location, Permissions } from 'expo';
+
 
 
 
 export default class Home extends Component {
+    state = {
+        mapRegion: null,
+        hasLocationPermission: false,
+        locationResult: null
+    };
+
+    componentDidMount() {
+        this._getLocationAsync();
+    }
+
+    _handleMapRegionChange = mapRegion => {
+        console.log(mapRegion);
+        this.setState({ mapRegion });
+    }
+
+    _getLocationAsync = async () => {
+        let { status } = await Permissions.askAsync(Permissions.LOCATION);
+        if (status !== 'granted') {
+            this.setState({
+                locationResult: 'Permission to access location was denied'
+            });
+        } else {
+            this.setState({ hasLocationPermission: true });
+        }
+
+        let location = await Location.getCurrentPositionAsync({});
+        this.setState({ locationResult: JSON.stringify(location) })
+    }
     render() {
         const styles = StyleSheet.create({
             title: {

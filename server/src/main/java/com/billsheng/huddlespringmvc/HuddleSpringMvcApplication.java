cd114e0316d8d6eb90a1e7beca4b266c430c8a18
Bill Sheng <billxsheng@gmail.com>
2019-03-11 17:59:18 -0400

    spring mvc setup

---
+++ server/src/main/java/com/billsheng/huddlespringmvc/HuddleSpringMvcApplication.java
@@ -0,0 +1,13 @@
+package com.billsheng.huddlespringmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuddleSpringMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HuddleSpringMvcApplication.class, args);
+	}
+
+}

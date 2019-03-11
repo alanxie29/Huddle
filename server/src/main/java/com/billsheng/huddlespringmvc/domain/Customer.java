c601778b706910ffaf8e95fdebbcec9221651b8f
Bill Sheng <billxsheng@gmail.com>
2019-03-11 18:13:06 -0400

    spring mvc setup

---
+++ server/src/main/java/com/billsheng/huddlespringmvc/domain/Customer.java
@@ -0,0 +1,22 @@
+package com.billsheng.huddlespringmvc.domain;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+//annotations
+@Data
+@Entity
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+
+
+}

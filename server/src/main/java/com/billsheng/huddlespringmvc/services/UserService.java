9a3cd65bb0fbf98b2ab567b27731ec9dd08b9e09
billxsheng <bsheng@opentext.com>
2019-03-12 10:38:35 -0400

    cant create new classes on work computer, finished user entity

--- server/src/main/java/com/billsheng/huddlespringmvc/services/UserService.java
+++ server/src/main/java/com/billsheng/huddlespringmvc/services/UserService.java
@@ -1,5 +1,7 @@
 package com.billsheng.huddlespringmvc.services;
 
 public interface UserService {
-
+    boolean canAuthenticate();
+    boolean userAlreadyExists();
+    boolean getUserByEmail();
 }
